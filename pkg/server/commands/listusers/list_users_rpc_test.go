89c4575ebefccdc6600ddd468c024b0e19b79b5f
Michael Gobaco <michael.gobaco@okta.com>
2025-05-13 09:38:29 -0700

    Add Panic Handling to ListUsers expandTTU

--- pkg/server/commands/listusers/list_users_rpc_test.go
+++ pkg/server/commands/listusers/list_users_rpc_test.go
@@ -3,6 +3,7 @@ package listusers
 import (
 	"context"
 	"fmt"
+	"sync/atomic"
 	"testing"
 	"time"
 
@@ -18,8 +19,11 @@ import (
 	"github.com/openfga/openfga/internal/mocks"
 	"github.com/openfga/openfga/internal/throttler/threshold"
 	"github.com/openfga/openfga/pkg/dispatch"
+	"github.com/openfga/openfga/pkg/logger"
+	serverconfig "github.com/openfga/openfga/pkg/server/config"
 	"github.com/openfga/openfga/pkg/storage"
 	"github.com/openfga/openfga/pkg/storage/memory"
+	"github.com/openfga/openfga/pkg/storage/storagewrappers"
 	storagetest "github.com/openfga/openfga/pkg/storage/test"
 	"github.com/openfga/openfga/pkg/testutils"
 	"github.com/openfga/openfga/pkg/tuple"
@@ -4028,4 +4032,72 @@ func TestListUsersRespectsConsistency(t *testing.T) {
 		})
 		require.NoError(t, err)
 	})
+}
+
+func TestListUsersUsersetsPanicExpandTTU(t *testing.T) {
+	t.Cleanup(func() {
+		goleak.VerifyNone(t)
+	})
+
+	tests := ListUsersTests{
+		{
+			name: "evaluate_userset_in_computed_relation_of_ttu_panic_expand_ttu",
+			req: &openfgav1.ListUsersRequest{
+				Object:   &openfgav1.Object{Type: "repo", Id: "fga"},
+				Relation: "reader",
+				UserFilters: []*openfgav1.UserTypeFilter{
+					{
+						Type:     "org",
+						Relation: "member",
+					},
+				},
+			},
+			model: `
+				model
+					schema 1.1
+				type user
+
+				type org
+					relations
+						define member: [user]
+						define admin: [org#member]
+
+				type repo
+					relations
+						define owner: [org]
+						define reader: admin from owner`,
+			tuples: []*openfgav1.TupleKey{
+				tuple.NewTupleKey("repo:fga", "owner", "org:x"),
+				tuple.NewTupleKey("org:x", "admin", "org:x#member"),
+				tuple.NewTupleKey("org:x", "member", "user:will"),
+			},
+			expectedUsers:     []string{},
+			expectedErrorMsg:  ErrPanic.Error(),
+			newListUsersQuery: NewListUsersQueryPanicExpandTTU,
+		},
+	}
+	tests.runListUsersTestCases(t)
+}
+
+func NewListUsersQueryPanicExpandTTU(ds storage.RelationshipTupleReader, contextualTuples []*openfgav1.TupleKey, opts ...ListUsersQueryOption) *listUsersQuery {
+	l := &listUsersQuery{
+		logger:                  logger.NewNoopLogger(),
+		resolveNodeBreadthLimit: serverconfig.DefaultResolveNodeBreadthLimit,
+		resolveNodeLimit:        serverconfig.DefaultResolveNodeLimit,
+		deadline:                serverconfig.DefaultListUsersDeadline,
+		maxResults:              serverconfig.DefaultListUsersMaxResults,
+		maxConcurrentReads:      serverconfig.DefaultMaxConcurrentReadsForListUsers,
+		wasThrottled:            new(atomic.Bool),
+		expandTTUDispatch: func(ctx context.Context, l *listUsersQuery, req *internalListUsersRequest, userObjectType, userObjectID, computedRelation string, foundUsersChan chan<- foundUser) expandResponse {
+			panic(ErrPanic)
+		},
+	}
+
+	for _, opt := range opts {
+		opt(l)
+	}
+
+	l.datastore = storagewrappers.NewRequestStorageWrapper(ds, contextualTuples, l.maxConcurrentReads)
+
+	return l
 }
